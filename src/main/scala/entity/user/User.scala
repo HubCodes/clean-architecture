a20ea24d0cda8873c7199cad8e456f2a15d33e09
HubCodes <krlrhkstk12@gmail.com>
2020-04-22 22:27:56 +0900

    Add: User.bio
    * 유저를 간략하게 소개하는 문구

--- src/main/scala/entity/user/User.scala
+++ src/main/scala/entity/user/User.scala
@@ -2,7 +2,7 @@ package entity.user
 
 import entity.article.Article
 
-final case class User(id: User.ID, nickname: String, articles: Set[Article.ID], accountID: Account.ID)
+final case class User(id: User.ID, nickname: String, articles: Set[Article.ID], accountID: Account.ID, bio: String)
 
 object User {
   type ID = entity.ID
